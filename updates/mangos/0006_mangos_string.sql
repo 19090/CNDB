be9d6ac2a97654218a9d5602af696dff0238f92e
ryancheung <ryancheung.go@gmail.com>
2016-05-17 14:54:37 +0800

    Add updates/mangos/0006_mangos_string.sql

---
+++ updates/mangos/0006_mangos_string.sql
@@ -0,0 +1,7 @@
+UPDATE `mangos_string` SET `content_loc4`='你的队伍成员数量说过，不适应于当前战场，请重新调整队伍后再尝试加入。' WHERE `entry`=720;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含离线成员，请移除后再尝试加入。' WHERE `entry`=727;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含对立阵营成员，请移除后再尝试加入。' WHERE `entry`=728;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含进入不同等级战场的成员，请移除后再尝试加入。' WHERE `entry`=729;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含已经进入该战场队列的成员，请移除后再尝试加入。' WHERE `entry`=730;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含战逃亡者，无法加入战场。' WHERE `entry`=731;
+UPDATE `mangos_string` SET `content_loc4`='你的队伍中包含已加入3个战场队列的成员，无法加入战场。' WHERE `entry`=732;
